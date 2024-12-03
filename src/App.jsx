8156562d4405219f36045c89eae5cb614620858c
Dawit yang <didekdnlt1996@gmail.com>
2024-12-03 23:57:24 +0900

    feat: 눈 내림/멈춤 기능 및 아이콘 전환 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
+import { useState } from "react";
 import { BsFillPlusCircleFill } from 'react-icons/bs';
+import { TbSnowflake, TbSnowflakeOff } from "react-icons/tb";
 import Snowfall from 'react-snowfall';
 
 import styles from './App.module.css';
@@ -14,30 +16,46 @@ import {
 import { useAlertStore, useTodoStore } from './store';
 
 const App = () => {
-  const {
-    isPopupOpen,
-    setSelectedTodo,
-    togglePopup,
-    deleteAllTodos
-  } = useTodoStore();
+  const { isPopupOpen, setSelectedTodo, togglePopup, deleteAllTodos } =
+    useTodoStore();
 
-  const { isAlertOpen, type, message, onConfirm, onCancel, deleteAllAlert } = useAlertStore();
+  const { isAlertOpen, type, message, onConfirm, onCancel, deleteAllAlert } =
+    useAlertStore();
 
-    const openAddTodoPopup = () => {
-      setSelectedTodo(null);
-      togglePopup();
-    };
-    
+  const [isSnowing, setIsSnowing] = useState(true);
+
+  const openAddTodoPopup = () => {
+    setSelectedTodo(null);
+    togglePopup();
+  };
+
+   const toggleSnow = () => {
+     setIsSnowing((prev) => !prev); 
+   };
+
   return (
     <div className={styles.todoApp}>
-      <Snowfall color="white" snowflakeCount={200} />
+      {isSnowing && <Snowfall color="white" snowflakeCount={200} />}
       <TodoBoard>
+        <div
+          className={styles.snowToggle}
+          onClick={toggleSnow}
+        >
+          {isSnowing ? (
+            <TbSnowflake size={24} />
+          ) : (
+            <TbSnowflakeOff size={24}  />
+          )}
+        </div>
         <TodoFilter />
         <div className={styles.todoBox}>
           <TodoCalender />
           <TodoList />
         </div>
-        <BsFillPlusCircleFill className={styles.todoAddBtn} onClick={openAddTodoPopup} />
+        <BsFillPlusCircleFill
+          className={styles.todoAddBtn}
+          onClick={openAddTodoPopup}
+        />
         <button
           className={classNames(styles.allDeleteBtn, styles.outlineBtn)}
           onClick={() => deleteAllAlert(deleteAllTodos)}
